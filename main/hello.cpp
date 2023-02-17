ab4bb006dbd73e9bf42e19a04bd33cf0bed5bbad
Ritvik333 <79704172+Ritvik333@users.noreply.github.com>
2023-02-17 22:51:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "good morning\n";
+  cout << "good morningg\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
